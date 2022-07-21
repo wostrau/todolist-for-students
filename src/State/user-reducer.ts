2751b88053e06a81386947d9e3f0eafa847c2155
wostrau <a.ostrovtsov@gmail.com>
2022-07-21 23:53:59 +0300

    repaired the user-reducer.test.ts // moved let userState to upper part of the function

--- src/State/user-reducer.ts
+++ src/State/user-reducer.ts
@@ -9,13 +9,12 @@ type ActionType = {
     [key: string]: any
 }
 export const userReducer = (state: StateType, action: ActionType): StateType => {
+    let newState = {...state};
     switch (action.type) {
         case 'INCREMENT-AGE':
-            let newState = {...state};
             newState.age = state.age + 1;
             return newState;
         case 'INCREMENT-CHILDREN-COUNT':
-            let newState = {...state};
             newState.childrenCount = state.childrenCount + 1;
             return newState;
         case 'CHANGE-NAME':
