081f00b3785465624dd858eb678e23c311238169
wostrau <a.ostrovtsov@gmail.com>
2022-09-08 23:04:45 +0300

    IN PROGRESS - 13th lesson of todolist-for-students / STOPPED at 1:00 / topic: API + Rest API + Axios

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -6,7 +6,10 @@ export default {
 };
 
 const settings = {
-    withCredentials: true
+    withCredentials: true,
+    headers: {
+        'API-KEY': '96e14868-2995-4951-a0b1-5ff5cded4fa9'
+    }
 };
 
 export const GetTodolist = () => {
@@ -23,7 +26,16 @@ export const GetTodolist = () => {
 };
 
 export const CreateTodolist = () => {
+    const [state, setState] = useState<any>(null);
 
+    useEffect(() => {
+        axios.post('https://social-network.samuraijs.com/api/1.1/todo-lists', {title: 'Alex todolist'}, settings)
+            .then((res) => {
+                setState(res.data);
+            });
+    }, []);
+
+    return <div>{JSON.stringify(state)}</div>
 };
 
 export const DeleteTodolist = () => {
